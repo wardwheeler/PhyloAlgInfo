40ce4b90fb074cf9133cfe4219646232806df37f
wardwheeler <wheeler@amnh.org>
2025-01-08 11:15:05 -0500

    UPdated error message for parser failuer on absent machine

--- Complexity/Parsers.hs
+++ Complexity/Parsers.hs
@@ -714,7 +714,7 @@ parseSections inSectionList =
     let machineModels = parseMachine <$> getElementString "machine" inSectionList
     in
     if length machineModels > 1 then errorWithoutStackTrace "Can only specify a single machine model"
-    else if null machineModels then errorWithoutStackTrace "Error in machine/block model/graph specification--perhaps missing final '};'"
+    else if null machineModels then errorWithoutStackTrace "Error in machine/block model/graph specification--No machine specification or perhaps missing final '};'"
     else
         --Find and pull Graph
       let (gMachineName, graphNameLocal, blockModels) = head machineModels
